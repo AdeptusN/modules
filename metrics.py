31799529d75ac6c12ceeafcc76d907aa3982d98a
EgorProkopov <egorprokopov216@gmail.com>
2023-04-30 17:17:11 +0300

    Начал переносить код в библу

---
+++ metrics.py
@@ -0,0 +1,34 @@
+'''Custom metrics by AdeptusN'''
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as functional
+
+
+class IoUMetricBin(nn.Module):
+    """
+    Metric for binary segmentation
+
+    Class of Intersection over Union metric
+
+    It is calculated as the ratio between the overlap of
+    the positive instances between two sets, and their mutual combined values
+
+    J(A, B) = |A and B| / |A or B| = |A and B| / (|A| + |B| - |A and B|)
+
+    """
+    def __init__(self):
+        super(IoUMetricBin, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        inputs = inputs.reshape(-1)
+        targets = targets.reshape(-1)
+
+        intersection = (inputs * targets).sum()
+        total = (inputs + targets).sum()
+        union = total - intersection
+
+        IoU = (intersection + smooth) / (union + smooth)
+
+        return IoU
+
